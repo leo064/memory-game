59cc3274bbee6aeadd8047c6f7caad59d6398249
leo064 <stalinlayla@gmail.com>
2021-05-21 14:15:51 -0300

    Fixing Issue #1
    Fisher-yates algorithm to shuffle cleanArray

--- Program.cs
+++ Program.cs
@@ -4,41 +4,58 @@ namespace test
 {
     class Program
     {
-        
         public static void Main()
         {
             string[] showArray = {"?", "?", "?", "?", "?", "?",
                          "?", "?", "?", "?", "?", "?"};
-            string[] numsArray = new string[12];
-            Random rnd = new Random();
-            for(int i=0; i<12; i++){
-                numsArray[i] = rnd.Next(1,7).ToString();
-            }
-            bool running = true;
+            string[] cleanArray = {"1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6"};
+            string[] shuffleArray = RandomizeArray(cleanArray);
             
+            bool running = true;
             while (running){
                 Console.Clear();
-                Console.Write(" # "+numsArray[0] +" # "+numsArray[1]+" # "+ numsArray[2] +
-                            "\n # "+ numsArray[3]+" # "+numsArray[4]+" # "+numsArray[5]+
-                            "\n # "+ numsArray[6]+" # "+numsArray[7]+" # "+numsArray[8]+
-                            "\n # "+ numsArray[9]+" # "+numsArray[10]+" # "+numsArray[11]);
+                //This write is just for test.
+                Console.Write(" # "+shuffleArray[0] +" # "+shuffleArray[1]+" # "+shuffleArray[2] +
+                            "\n # "+ shuffleArray[3]+" # "+shuffleArray[4]+" # "+shuffleArray[5]+
+                            "\n # "+ shuffleArray[6]+" # "+shuffleArray[7]+" # "+shuffleArray[8]+
+                            "\n # "+ shuffleArray[9]+" # "+shuffleArray[10]+" # "+shuffleArray[11]);
                 for(int j = 0; j<12; j++){
+                    //to show array and break lines
                     if(j%3==0)
                         Console.WriteLine();
                     Console.Write($" [{showArray[j]}] ");
                 }
                 Console.WriteLine("\nQual carta deseja virar? (0-11)");
                 int sideCard = int.Parse(Console.ReadLine());
-                showArray = AlteraIndice(sideCard, showArray, numsArray);
+                showArray = ChangeIndex(sideCard, showArray, shuffleArray);
                 
                 Console.Write(showArray+ "\n");
             }
         }
-        static string[] AlteraIndice(int n, string[] show, string[] nums){
+        static string[] ChangeIndex(int n, string[] show, string[] sArray){
             Console.Clear();
-            show[n] = nums[n];
+            show[n] = sArray[n];
             return show;
         }
 
+        static string[] RandomizeArray(string[] array){
+            /*
+            using the "Fisher-yates algorithm :"
+            Basicamente eu gero um número randômico de 0 a n--. Esse número
+            vai representar o índice que eu vou alterar para randomizar os elementos
+            de um vetor.
+            Por fim faço um processo de troca de lugares entre a posição randomizada com 
+            a posição atual do meu vetor e... voilá
+            */
+            Random rnd = new Random();
+            int n = array.Length;
+            while(n > 0){
+                int k = rnd.Next(0, n--);
+                string aux = array[n];
+                array[n] = array[k];
+                array[k] = aux;
+            }
+            return array;
+        }
    }
 }
